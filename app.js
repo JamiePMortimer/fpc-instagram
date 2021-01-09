3d218680a3aa6aa92ea53fde2ed4d515112d2bbb
JamiePMortimer <16468666+JamiePMortimer@users.noreply.github.com>
2021-01-09 20:36:55 +0000

    Working on login button

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const passwordHandler = document.getElementById('hideShow');
 const inputHandler = document.getElementById('pword');
+const usernameInput = document.getElementById('userName');
+const loginListener = document.querySelector('.login');
+const loginBtn = document.querySelector('.login-btn');
 
 passwordHandler.addEventListener('click', buttonToggler)
 
@@ -12,3 +15,10 @@ function buttonToggler (){
   inputHandler.type = 'password'
 }
 }
+
+loginListener.addEventListener('input', ()=>{
+  if (usernameInput.value !== '' && inputHandler.value !== ''){
+    loginBtn.classList.remove('disabled')
+    // loginBtn.classList.toggle('disabled')
+  }
+})
